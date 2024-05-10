4a2b4156c536365c5626d1a3565650c7c80a1950
Saketh Katta <sakethkatta@gmail.com>
2024-05-09 21:55:59 -0500

    Made adjustments to mario

--- mario.js
+++ mario.js
@@ -65,19 +65,20 @@ function drawMario() {
     context.translate(0, 2 * canvas.height / 3, canvas.width);
     context.scale(mario.scale, mario.scale);
     context.translate(0, -2 * canvas.height / 3, canvas.width);
+    // Green
     context.fillStyle = "rgb(139,115,4)";
     context.fillRect(mario.x, 2 * canvas.height / 3 - 12 - mario.y, 50, 12.5);
-    context.fillRect(mario.x + 12.5, 2 * canvas.height / 3 - 24 - mario.y, 37.5, 12.5);
-    context.fillRect(mario.x + 100, 2 * canvas.height / 3 - 12 - mario.y, 50, 12.5);
-    context.fillRect(mario.x + 100, 2 * canvas.height / 3 - 24 - mario.y, 37.5, 12.5);
-    context.fillRect(mario.x + 37.5, 2 * canvas.height / 3 - 72 - mario.y, 75, 12.5);
-    context.fillRect(mario.x, 2 * canvas.height / 3 - 84 - mario.y, 50, 12.5);
-    context.fillRect(mario.x + 100, 2 * canvas.height / 3 - 84 - mario.y, 50, 12.5);
-    context.fillRect(mario.x + 12.5, 2 * canvas.height / 3 - 96 - mario.y, 50, 12.5);
-    context.fillRect(mario.x + 87.5, 2 * canvas.height / 3 - 96 - mario.y, 50, 12.5);
-    context.fillRect(mario.x + 25, 2 * canvas.height / 3 - 108 - mario.y, 25, 12.5);
-    context.fillRect(mario.x + 100, 2 * canvas.height / 3 - 108 - mario.y, 25, 12.5);
-    context.fillRect(mario.x + 65, 2 * canvas.height / 3 - 108 - mario.y, 20, 25);
+    context.fillRect(mario.x + 12.5, 2 * canvas.height / 3 - 24 - mario.y, 37.5, 14);
+    context.fillRect(mario.x + 100, 2 * canvas.height / 3 - 12 - mario.y, 50, 14);
+    context.fillRect(mario.x + 100, 2 * canvas.height / 3 - 24 - mario.y, 37.5, 14);
+    context.fillRect(mario.x + 37.5, 2 * canvas.height / 3 - 72 - mario.y, 75, 14);
+    context.fillRect(mario.x, 2 * canvas.height / 3 - 84 - mario.y, 50, 14);
+    context.fillRect(mario.x + 100, 2 * canvas.height / 3 - 84 - mario.y, 50, 14);
+    context.fillRect(mario.x + 12.5, 2 * canvas.height / 3 - 96 - mario.y, 50, 14);
+    context.fillRect(mario.x + 87.5, 2 * canvas.height / 3 - 96 - mario.y, 50, 14);
+    context.fillRect(mario.x + 25, 2 * canvas.height / 3 - 108 - mario.y, 25, 14);
+    context.fillRect(mario.x + 100, 2 * canvas.height / 3 - 108 - mario.y, 25, 14);
+    context.fillRect(mario.x + 65, 2 * canvas.height / 3 - 108 - mario.y, 20, 28);
     if(mario.left == 1) {
         context.fillRect(mario.x + 25, 2 * canvas.height / 3 - 132 - mario.y, 100, 12.5);
         context.fillRect(mario.x + 12.5, 2 * canvas.height / 3 - 144 - mario.y, 112.5, 12.5);
@@ -90,47 +91,49 @@ function drawMario() {
         context.fillRect(mario.x + 25, 2 * canvas.height / 3 - 156 - mario.y, 100, 12.5);
         context.fillRect(mario.x + 37.5, 2 * canvas.height / 3 - 168 - mario.y, 75, 12.5);
     }
+    // Red
     context.fillStyle = "rgb(216,41,0)";
-    context.fillRect(mario.x + 25, 2 * canvas.height / 3 - 36 - mario.y, 37.5, 12.5);
-    context.fillRect(mario.x + 87.5, 2 * canvas.height / 3 - 36 - mario.y, 37.5, 12.5);
-    context.fillRect(mario.x + 37.5, 2 * canvas.height / 3 - 48 - mario.y, 75, 12.5);
-    context.fillRect(mario.x + 50, 2 * canvas.height / 3 - 60 - mario.y, 50, 12.5);
-    context.fillRect(mario.x + 50, 2 * canvas.height / 3 - 72 - mario.y, 50, 12.5);
-    context.fillRect(mario.x + 50, 2 * canvas.height / 3 - 84 - mario.y, 50, 12.5);
-    context.fillRect(mario.x + 50, 2 * canvas.height / 3 - 108 - mario.y, 15, 25);
-    context.fillRect(mario.x + 85, 2 * canvas.height / 3 - 108 - mario.y, 15, 25);
+    context.fillRect(mario.x + 25, 2 * canvas.height / 3 - 36 - mario.y, 37.5, 14);
+    context.fillRect(mario.x + 87.5, 2 * canvas.height / 3 - 36 - mario.y, 37.5, 14);
+    context.fillRect(mario.x + 37.5, 2 * canvas.height / 3 - 48 - mario.y, 75, 14);
+    context.fillRect(mario.x + 50, 2 * canvas.height / 3 - 60 - mario.y, 50, 14);
+    context.fillRect(mario.x + 50, 2 * canvas.height / 3 - 72 - mario.y, 50, 14);
+    context.fillRect(mario.x + 50, 2 * canvas.height / 3 - 84 - mario.y, 50, 14);
+    context.fillRect(mario.x + 50, 2 * canvas.height / 3 - 108 - mario.y, 15, 28);
+    context.fillRect(mario.x + 85, 2 * canvas.height / 3 - 108 - mario.y, 15, 28);
     if(mario.left == 1) {
-        context.fillRect(mario.x + 25, 2 * canvas.height / 3 - 180 - mario.y, 87.5, 12.5);
-        context.fillRect(mario.x + 37.5, 2 * canvas.height / 3 - 192 - mario.y, 62.5, 12.5);
+        context.fillRect(mario.x + 25, 2 * canvas.height / 3 - 180 - mario.y, 87.5, 14);
+        context.fillRect(mario.x + 37.5, 2 * canvas.height / 3 - 192 - mario.y, 62.5, 14);
     }
     else {
-        context.fillRect(mario.x + 37.5, 2 * canvas.height / 3 - 180 - mario.y, 87.5, 12.5);
-        context.fillRect(mario.x + 50, 2 * canvas.height / 3 - 192 - mario.y, 62.5, 12.5);
+        context.fillRect(mario.x + 37.5, 2 * canvas.height / 3 - 180 - mario.y, 87.5, 14);
+        context.fillRect(mario.x + 50, 2 * canvas.height / 3 - 192 - mario.y, 62.5, 14);
     }
+    // Orange
     context.fillStyle = "rgb(253,152,56)";
-    context.fillRect(mario.x, 2 * canvas.height / 3 - 48 - mario.y, 38, 12.5);
-    context.fillRect(mario.x + 112, 2 * canvas.height / 3 - 48 - mario.y, 38, 12.5);
-    context.fillRect(mario.x, 2 * canvas.height / 3 - 60 - mario.y, 50, 12.5);
-    context.fillRect(mario.x + 100, 2 * canvas.height / 3 - 60 - mario.y, 50, 12.5);
-    context.fillRect(mario.x, 2 * canvas.height / 3 - 72 - mario.y, 38, 12.5);
-    context.fillRect(mario.x + 112, 2 * canvas.height / 3 - 72 - mario.y, 38, 12.5);
-    context.fillRect(mario.x + 37.5, 2 * canvas.height / 3 - 120 - mario.y, 75, 12.5);
-    context.fillRect(mario.x + 62.5, 2 * canvas.height / 3 - 168 - mario.y, 25, 37.5);
+    context.fillRect(mario.x, 2 * canvas.height / 3 - 48 - mario.y, 38, 14);
+    context.fillRect(mario.x + 112, 2 * canvas.height / 3 - 48 - mario.y, 38, 14);
+    context.fillRect(mario.x, 2 * canvas.height / 3 - 60 - mario.y, 50, 14);
+    context.fillRect(mario.x + 100, 2 * canvas.height / 3 - 60 - mario.y, 50, 14);
+    context.fillRect(mario.x, 2 * canvas.height / 3 - 72 - mario.y, 38, 14);
+    context.fillRect(mario.x + 112, 2 * canvas.height / 3 - 72 - mario.y, 38, 14);
+    context.fillRect(mario.x + 37.5, 2 * canvas.height / 3 - 120 - mario.y, 75, 13);
+    context.fillRect(mario.x + 62.5, 2 * canvas.height / 3 - 166 - mario.y, 25, 37.5);
     if(mario.left == 1) {
-        context.fillRect(mario.x + 62.5, 2 * canvas.height / 3 - 132 - mario.y, 37.5, 12.5);
-        context.fillRect(mario.x + 50.5, 2 * canvas.height / 3 - 144 - mario.y, 12.5, 12.5);
-        context.fillRect(mario.x + 12.5, 2 * canvas.height / 3 - 144 - mario.y, 25.5, 12.5);
+        context.fillRect(mario.x + 62.5, 2 * canvas.height / 3 - 132 - mario.y, 37.5, 13.5);
+        context.fillRect(mario.x + 50.5, 2 * canvas.height / 3 - 145 - mario.y, 13, 13);
+        context.fillRect(mario.x + 12.5, 2 * canvas.height / 3 - 145 - mario.y, 25.5, 13);
         context.fillRect(mario.x + 25, 2 * canvas.height / 3 - 156 - mario.y, 25.5, 12.5);
-        context.fillRect(mario.x + 37.5, 2 * canvas.height / 3 - 168 - mario.y, 13, 12.5);
-        context.fillRect(mario.x + 100, 2 * canvas.height / 3 - 156 - mario.y, 13, 25);
+        context.fillRect(mario.x + 37.5, 2 * canvas.height / 3 - 166 - mario.y, 13, 13);
+        context.fillRect(mario.x + 100, 2 * canvas.height / 3 - 156 - mario.y, 13, 26);
     }
     else {
-        context.fillRect(mario.x + 50, 2 * canvas.height / 3 - 132 - mario.y, 37.5, 12.5);
-        context.fillRect(mario.x + 87, 2 * canvas.height / 3 - 144 - mario.y, 12.5, 12.5);
-        context.fillRect(mario.x + 112.5, 2 * canvas.height / 3 - 144 - mario.y, 25.5, 12.5);
+        context.fillRect(mario.x + 50, 2 * canvas.height / 3 - 132 - mario.y, 37.5, 13.5);
+        context.fillRect(mario.x + 86, 2 * canvas.height / 3 - 145 - mario.y, 13, 13);
+        context.fillRect(mario.x + 112.5, 2 * canvas.height / 3 - 145 - mario.y, 25.5, 13);
         context.fillRect(mario.x + 100, 2 * canvas.height / 3 - 156 - mario.y, 25.5, 12.5);
-        context.fillRect(mario.x + 100, 2 * canvas.height / 3 - 168 - mario.y, 13, 12.5);
-        context.fillRect(mario.x + 37.5, 2 * canvas.height / 3 - 156 - mario.y, 13, 25);
+        context.fillRect(mario.x + 100, 2 * canvas.height / 3 - 166 - mario.y, 13, 13);
+        context.fillRect(mario.x + 37.5, 2 * canvas.height / 3 - 156 - mario.y, 13, 26);
     }
     context.restore();
 }
